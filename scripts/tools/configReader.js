691e013a6be3e967567c2463a03578fe1ccdd04a
minosletitgo <380122248@qq.com>
2024-12-10 19:22:35 +0800

    1

--- scripts/tools/configReader.js
+++ scripts/tools/configReader.js
@@ -3,26 +3,11 @@
 const fs = require("fs");
 const path = require("path");
 
-// 定义合约的静态属性
-const STATIC_CONFIG_FILE_PATH = path.join(
-  __dirname,
-  "../../configManual/contractStatic.json"
-);
-
 // 定义合约的参数配置
 const PARAMS_CONFIG_FILE_PATH = path.join(
     __dirname,
     "../../configManual/contractParams.json"
   );
-
-function loadContractStatic() {
-  try {
-    return JSON.parse(fs.readFileSync(STATIC_CONFIG_FILE_PATH, "utf8"));
-  } catch (error) {
-    console.error("Error loading static config file:", error);
-    throw error;
-  }
-}
 
 function loadContractParams() {
     try {
@@ -35,6 +20,5 @@ function loadContractParams() {
 
 // 导出函数以供外界调用
 module.exports = {
-  loadContractStatic,
   loadContractParams,
 };
