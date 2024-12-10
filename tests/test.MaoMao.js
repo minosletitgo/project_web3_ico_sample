e38beddfe17f7ed92796347fd5138992c89fe810
minosletitgo <380122248@qq.com>
2024-12-10 16:36:29 +0800

    1

--- tests/test.MaoMao.js
+++ tests/test.MaoMao.js
@@ -4,7 +4,7 @@ const contractABI = require("../abi/contracts/myTokens/MaoMao.sol/MaoMao.json");
 require("dotenv").config();
 
 describe(" ", function () {
-  let contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+  let contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
   let abi = contractABI;
   let contract;
 
@@ -21,7 +21,7 @@ describe(" ", function () {
     // 查询指定地址的代币数额
     const balanceOfAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
     const balanceOf = await contract.balanceOf(balanceOfAddress);
-    logger.info(`${balanceOfAddress} -> MaoMao: ${balanceOf}`);
+    logger.info(`${hre.network.name} -> ${balanceOfAddress} -> MaoMao: ${balanceOf}`);
   });
 });
 
